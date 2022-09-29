372b6104faa68ed15d9ae2a815cbd2899250c732
Real-Artisan <iwenyadaniel12@gmail.com>
2022-09-29 22:55:05 +0100

    front page Layout

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div className='name' >Enter name</div>
-        <input placeholder='Name' />
-        <button>Go</button>
+        <h2 className='name' >Enter Username</h2>
+        <TextField
+          className='input'
+          required
+          id="outlined-required"
+          label="Name"
+        />
+        <br></br>
+        <Button  variant="contained" >Go</Button>
       </header>
     </div>
   );
