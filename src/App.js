9ee3980bd3011da1f35e6de792735ef73b8c45d2
Real-Artisan <iwenyadaniel12@gmail.com>
2022-09-30 00:09:55 +0100

    login and move to chat room

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 
 function App() {
+  const user = sessionStorage.getItem('me');
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <h2 className='name' >Enter Username</h2>
-        <TextField
-          className='input'
-          required
-          id="outlined-required"
-          label="Name"
-        />
-        <br></br>
-        <Button  variant="contained" >Go</Button>
+      <header>
+        
       </header>
+      <section>
+        {user ? <ChatRoom /> : <Login />}
+      </section>
     </div>
   );
+}
+
+function Login() {
+
+  const login = () =>
+  {
+    sessionStorage.setItem('me', userName);
+    window.location.reload();
+
+  }
+  const [userName, setUserName] = useState('');
+return (
+  <header className='App-header'>
+    <h2 className='name' >Enter Username</h2>
+        <TextField
+          onChange={(name) =>setUserName(name.target.value) }
+          className='input'
+          required
+          value={userName}
+          id="outlined-required"
+          label="Name"
+
+        />
+        <br></br>
+        <Button onClick={login} variant="contained" >Go</Button>
+  </header>
+
+)
+}
+
+function ChatRoom() {
+  return (
+    <h1>Welcome to chatroom</h1>
+  )
 }
 
 export default App;
